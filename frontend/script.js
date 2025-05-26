4bb6712378b23bde57de08be98e4a3709df15e99
samsyl1 <samuel.s.young19@gmail.com>
2025-05-26 16:41:53 +1000

    Prevent flag button from appearing on system message 2

--- frontend/script.js
+++ frontend/script.js
@@ -134,7 +134,7 @@ const header=document.getElementById('header');
         !text.includes("Thank you for chatting!") &&
         !text.includes("To chat with us") &&
         !text.includes("Rate your experience") &&
-        !text.includes("Thanks for visiting!")) {
+        !text.includes("Thanks for visiting")) {
         lastBotResponse = text;
     }
 
